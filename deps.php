7b2c664da1590a0b59e66ae0178fa8b76d0fd0a3
Konrad <konradzuse.dm@gmail.com>
2021-03-12 16:26:20 -0500

    created source tree files and folders and added bootstrap 4 / jQuery and Datatables libs and linked on deps.php

---
+++ deps.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="libs/bootstrap/bootstrap.min.css">
+<script src="libs/bootstrap/jquery.min.js"></script>
+<script src="libs/bootstrap/popper.min.js"></script>
+<script src="libs/bootstrap/bootstrap.min.js"></script>
