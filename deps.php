d70e5aba48442bdfce12c0923310293e023f4ce7
Konrad <konradzuse.dm@gmail.com>
2021-03-13 05:16:13 -0500

    added deps by npm

--- deps.php
+++ deps.php
@@ -1,4 +1,7 @@
 <link rel="stylesheet" href="libs/bootstrap/bootstrap.min.css">
+
+
+
 <script src="libs/bootstrap/jquery.min.js"></script>
 <script src="libs/bootstrap/popper.min.js"></script>
 <script src="libs/bootstrap/bootstrap.min.js"></script>
