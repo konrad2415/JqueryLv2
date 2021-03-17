f43ed391c4333fc5a296134fc320f9698e5b7503
Konrad <konradzuse.dm@gmail.com>
2021-03-17 18:09:13 -0500

    Added all libs with no errors -- jquery bootstrap+css+jqJs popperjs+css dataTables+Css alertifyJs+css

--- deps.php
+++ deps.php
@@ -1,7 +1,20 @@
-<link rel="stylesheet" href="libs/bootstrap/bootstrap.min.css">
+<link rel="stylesheet" href="libs/bootstrap/bootstrap.min.css" />
+
+<link rel="stylesheet" href="libs/datatable/bootstrap.css" />
+<link rel="stylesheet" href="libs/datatable/dataTables.bootstrap4.min.css" />
 
 
+<link rel="stylesheet" href="libs/alertify/css/alertify.min.css" />
+<link rel="stylesheet" href="libs/alertify/css/themes/bootstrap.min.css" />
 
-<script src="libs/bootstrap/jquery.min.js"></script>
+<script src="libs/bootstrap/jquery-3.2.1.min.js"></script>
+
 <script src="libs/bootstrap/popper.min.js"></script>
+
+<script src="libs/alertify/alertify.min.js"></script>
+
 <script src="libs/bootstrap/bootstrap.min.js"></script>
+
+<script src="libs/datatable/jquery.dataTables.min.js"></script>
+
+<script src="libs/datatable/dataTables.bootstrap4.min.js"></script>
