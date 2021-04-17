30ecf44fc298de88289c439cc98c265c34dd088d
Konrad <konradzuse.dm@gmail.com>
2021-04-17 05:26:15 -0400

    some tests over  Connect() object with DB

--- class/connect.php
+++ class/connect.php
@@ -14,7 +14,7 @@
               private  $conn;  
               private  $query;     
               
-              public function  __construct($serv="localhost",$user="konra",$key="Hall0w33nday",$db="juegos"){
+              public function  __construct($serv="localhost",$user="konrad",$key="Hall0w33nday",$db="juegos"){
                      $this->server  = $serv ;
                      $this->usr     = $user;
                      $this->pass    = $key;
@@ -25,13 +25,35 @@
               public function  __destruct(){}
 
               public function doConnect(){ 
-                    $this->conn = new mysqli($server,$usr,$pass,$db); // server | user | pass-code | database
-                    if ($this->conn ->connect_error) die ('ERROR!! Connection failed:'.$this->conn->connect_error()) ; 
-                    else return $this->conn;
+                    $this->conn = new mysqli($this->server,$this->usr,$this->pass,$this->db); // server | user | pass-code | database
+                    if ($this->conn ->connect_error) die ('ERROR!! Connection failed:'.$this->conn->connect_error) ; 
+                        else return $this->conn;
               }
 
+              public function doClose(){ return $this->conn->close();}
+              
+              // CRUD  onto database methods
+              public function dbCreate($sqlR){
 
+              }
 
+              public function dbRead($sqlR){
+                     $this->query    = $sqlR;
+                       $result = mysqli_query($this->conn,$this->query);
+                       echo "<br/><br/>";   
+                       var_dump($result);
+                       echo "<br/>Sqli Query ok<br/>";   
+
+                        //$this->conn -> close();    
+                 
+              }
+
+              public function dbUpdate($sqlW){
+
+              }
+              public function dbDelete($sqlW){
+
+              }
 
               // Set internal methods
               public function setServer($srv){
@@ -66,12 +88,20 @@
        }
 
 
-       $t = new CNT;
+       $t = new Connect();
        //$t -> doConnect();
        echo $t->getDbuser();
        echo "<br/>".$t->getDbname();
+       if($t-> doConnect())echo "<br/>Connected successfully!";
+        
+        
+        $t->dbRead("SELECT id_juego,
+        nombre,
+        anio,
+        empresa
+    FROM t_juegos");
 
-
+if($t->doClose())echo "<br/>Connection closed successfully";
        /*
        
         if ($conn ->connect_error) die ('ERROR!! Connection failed:'.$conn->connect_error()) ;
